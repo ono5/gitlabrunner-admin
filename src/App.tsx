f6f3217c2ae4bb7e7144f91158a4297eb8f82567
ono5 <kuroneko_head_lord@yahoo.co.jp>
2022-03-17 04:46:03 +0900

    Add About Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import About from "./pages/About";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
+      {/* https://github.com/remix-run/react-router/blob/main/docs/getting-started/tutorial.md */}
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
       </Routes>
     </Router>
   );
