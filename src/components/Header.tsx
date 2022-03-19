9cf756c425932c94ff006a001fa0fab93d63d504
ono5 <kuroneko_head_lord@yahoo.co.jp>
2022-03-19 16:22:21 +0900

    Add contents in the header section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,6 +10,23 @@ const Header = () => {
 			<div className="header__video">
 				<MyYouTube />
 			</div>
+			<div className="header__contents">
+				<div className="container">
+					<div className="header__contents__text">
+						<div className="header__contents__text__child">
+							<h1 className="header__contents__text__child__h1">
+								Admin GitLab Runner
+							</h1>
+							<p className="header__contents__text__child__p">
+								Come and join us we admin gitlab runner
+							</p>
+							<div className="header__contents__text__child__link">
+								<button>Get Started</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	)
 }
