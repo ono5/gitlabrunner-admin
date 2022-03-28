ae0a80f8c3ef2dbc15879acb6b7ac97acbf4e0cb
ono5 <kuroneko_head_lord@yahoo.co.jp>
2022-03-28 10:11:47 +0900

    crate bootom footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,10 +4,10 @@ import FooterLinks from "./FooterLinks"
 const Footer = () => {
 	// 画像をSCSSから設定できないため、ここで設定する
 	const footerStyles = {
-		// background: 'url(/assets/images/footer/footer.png)',
+		background: 'url(/assets/images/footer/footer.png)',
 		backgroundAttachment: 'fixed',
 		backgroundSize: 'cover',
-		backgroundRepeat: 'no-repeat',
+		backgroundRepeat: 'no-repeat'
 	}
 	return (
 		<footer className="footer" style={footerStyles}>
@@ -22,6 +22,9 @@ const Footer = () => {
 								<FooterLinks />
 							</div>
 						</div>
+					</div>
+					<div className="footer__bottom">
+						<p>&copy; copy right gitlab projects {new Date().getFullYear()}</p>
 					</div>
 				</div>
 			</div>
